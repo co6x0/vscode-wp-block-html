7494f055eb51f0585f6804d2247b45cc381757a2
naokomura <nao.komura@sixaxd.com>
2023-02-17 01:24:12 +0900

    add snippets

--- src/utils/genSnippets.ts
+++ src/utils/genSnippets.ts
@@ -1,13 +1,30 @@
-// import { html } from "./core-block-ref-20230216";
-// console.log(html);
+// https://developer.wordpress.org/block-editor/reference-guides/core-blocks/
+const elements = document.getElementsByClassName("toc-header");
+const arrayElements = Object.values(elements);
+const snippets = arrayElements.map((element) => {
+  const headingElement = element.lastChild as HTMLHeadingElement;
+  const paragraphElement = element.nextElementSibling as HTMLParagraphElement;
+  if (!headingElement || !paragraphElement) {
+    return {};
+  }
+  return {
+    [headingElement.textContent ?? "_"]: {
+      prefix: [`wp:${headingElement.id}`],
+      body: [`<!-- wp:${headingElement.id} $1/-->`],
+      description: `${paragraphElement.innerText.replace(
+        " (Source)",
+        ""
+      )} https://developer.wordpress.org/block-editor/reference-guides/core-blocks/#${
+        headingElement.id
+      }`,
+    },
+  };
+});
 
-// const newElements = elements.map((element) => {
-//   return {
-//     [`wp:${element.id}`]: {
-//       prefix: `wp:${element.id}`,
-//       body: [`<!-- wp:${element.id} $1/-->`],
-//       snippetSuggestions: "top",
-//       description: `${element.baseURI}`,
-//     },
-//   };
-// });
+const target = {};
+for (const snippet of snippets) {
+  Object.assign(target, snippet);
+}
+
+const snippetsJson = JSON.stringify(target);
+console.log(snippetsJson);
