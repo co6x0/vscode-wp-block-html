9c61b0d667341193a81b2a58eb4bca90bbc8aaf1
Nao Komura <nao.komura@sixaxd.com>
2023-02-16 23:30:03 +0900

    WIP add snippets

---
+++ src/utils/genSnippets.ts
@@ -0,0 +1,13 @@
+// import { html } from "./core-block-ref-20230216";
+// console.log(html);
+
+// const newElements = elements.map((element) => {
+//   return {
+//     [`wp:${element.id}`]: {
+//       prefix: `wp:${element.id}`,
+//       body: [`<!-- wp:${element.id} $1/-->`],
+//       snippetSuggestions: "top",
+//       description: `${element.baseURI}`,
+//     },
+//   };
+// });
